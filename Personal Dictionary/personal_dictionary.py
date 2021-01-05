4a829dec82eea83168fd6b803d4a0f3ed7dd8e6c
Khyati Sanghvi <64585159+khyatisanghvi@users.noreply.github.com>
2021-01-05 15:12:34 +0530

    Update personal_dictionary.py
    Updated the GUI look and feel.
    
    Modified Database class for Add, Delete , Clear and Exit Buttons.

--- Personal Dictionary/personal_dictionary.py
+++ Personal Dictionary/personal_dictionary.py
@@ -1,134 +1,212 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+import traceback
+from sqlite3 import Error
 from tkinter import *
-import dbms as db
-from functools import partial
-from tkinter import messagebox
+from tkinter import ttk, Scrollbar, messagebox
 
 
-window=Tk()
-window.title("GUI DICTIONARY")
+class Database:
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS dictionary(id integer PRIMARY KEY,Word text,Meaning text)")
+        self.conn.commit()
 
-class Table: 
-      
-    def __init__(self,root, lst): 
-        total_rows = len(lst) 
-        total_columns = len(lst[0])
-        root=Tk()
-        root.title("Data")  
-        # code for creating table 
-        for i in range(total_rows): 
-            for j in range(1,total_columns): 
-                if j == 1:
-                    self.e = Entry(root, fg='blue', font=('Arial',16,'bold'))
-                else:
-                    self.e = Entry(root, fg='blue', font=('Arial',16,'bold'), width=50) 
-                  
-                self.e.grid(row=i, column=j) 
-                self.e.insert(END, lst[i][j]) 
-                # self.e.command(readWord(self.e.get()))
-        root.mainloop()
- 
+    def fetch(self):
+        self.cur.execute("SELECT * FROM dictionary")
+        rows = self.cur.fetchall()
+        return rows
 
-# initialize (connection established to the database)
-dbms_inst = db.dbms("dictionary", "Dict_personal.db")
+    def insert(self, word, meaning):
+        self.cur.execute("INSERT INTO dictionary VALUES (NULL, ?, ?)", (word, meaning))
+        self.conn.commit()
 
-# create a new table
-list = ["Word", "Meaning"]    
+    def remove(self, id):
+        self.cur.execute("DELETE FROM dictionary WHERE id=?", (id,))
+        self.conn.commit()
 
-sql_create_table = dbms_inst.get_sql_for_creating_table(list)  
+    def update(self, id, word, meaning):
+        self.cur.execute("UPDATE dictionary SET word = ?, meaning = ? WHERE id = ?",
+                         (word, meaning, id))
+        self.conn.commit()
 
-print(sql_create_table)
-print()
-dbms_inst.create_table(sql_create_table)
-
-# insert data            
-sql = dbms_inst.get_sql_for_creating_entry(list)            
-print(sql)
-print()
-
-def mean():
-    t.insert(END,"data")
-    
-def reset():
-    word.set("")
-    meaning.set("")
-    t.delete(1.0,END)
-    t1.delete(1.0,END)
-    
-def exit():
-    window.destroy()
-    
-def addWord(t,t1):
-    word_to_add = t.get()
-    meaning = t1.get()
-    
-    try:
-        entry = (word_to_add, meaning)
-        cursor = dbms_inst.create_entry(sql, entry)
-        msg = messagebox.showinfo( "Message", "Word added")
-    except Exception:
-        str = traceback.print_exc()
-        msg = messagebox.showinfo( "Message", str)
-        
-    reset()
-        
-        
-def showWords():
-    word_list = dbms_inst.select_entries("SELECT * FROM dictionary")
-    print(word_list)
-    
-    # take the data            
-    lst = word_list
-       
-    # find total number of rows and 
-    # columns in list
-    
-    ret = Table(window,lst) 
-    
-def readWord(word):
-    print("Hello")
-    print(word)
-    
-    
+    def __del__(self):
+        self.conn.close()
 
 
+class ConfigureGUI():
+    """[summary]
+    """
 
-# Entry Box Label
-b = Label(window, text="Enter Word")
-b.config(font=(30))
-b.grid(row=1,column=1)
+    def __init__(self, root):
+        # Configure the root object for the Application
+        self.root = root
+        self.root.title(102 * " " + "GUI DICTIONARY")
+        self.root.geometry("720x770+300+0")
+        self.root.resizable(width=False, height=False)
+        self.root.config(background="silver")
 
-# Entry Box
-word_to_find = StringVar()
-e = Entry(window, textvariable = word_to_find, width=20)
-e.grid(row=1, column=2)
+        # Create an instance of Database Class
+        self.dbms_inst = Database("Dict_personal.db")
 
-# Button
-b1 = Button(window, text="Find!", bg="skyblue", width="10", command=showWords)
-# b1.config(font=(20))
-b1.grid(row=1,column=3)
+        # ======================VARIABLES==============================
+        word_to_add = StringVar()
+        word_description = StringVar()
+        self.id = 1
+        self.iid = 0
 
-# Text Box
-word = StringVar()
-t = Entry(window, textvariable = word, width = 30)
-t.grid(row=2, column=1,  columnspan = 2)
+        # ======================FRAMES==============================
+        # Create a Main Frame
+        main_frame = Frame(self.root, bd=10, width=770, height=700,
+                           relief=RIDGE, bg='cadet blue')
+        main_frame.grid()
 
-meaning = StringVar()
-t1 = Entry(window, textvariable = meaning, width = 60)
-t1.grid(row=2, column=3,  columnspan = 4)
+        # Create a Title Frame
+        title_frame = Frame(main_frame, bd=7, padx=10, pady=10, width=770,
+                            height=100, relief=RIDGE)
+        title_frame.grid(row=0, column=0, padx=2, pady=2, sticky="nsew")
 
-# Add Button
-addWord = partial(addWord, t, t1)
-add = Button(window, text="Add", bg="skyblue", width=10, command=addWord)
-add.grid(row=1,column=4)
+        # Create a Data Frame
+        data_frame = Frame(main_frame, bd=5, width=770, height=700,
+                           padx=10, pady=10, relief=RAISED)
+        data_frame.grid(row=1, column=0, padx=2, pady=2, sticky="nsew")
+
+        # ======================LABEL WIDGET=============================
+        self.lblWordToAdd = Label(title_frame, font=('Arial', 12, 'bold'),
+                                  text="Enter your word ",
+                                  padx=2, pady=2, borderwidth=0)
+        self.lblWordToAdd.grid(row=0, column=0, sticky=W, padx=5)
+
+        self.lblDescription = Label(title_frame, font=('Arial', 12, 'bold'),
+                                    text="Enter description ",
+                                    padx=2, pady=2, borderwidth=0)
+        self.lblDescription.grid(row=1, column=0, sticky=W, padx=5)
+
+        # ======================ENTRY WIDGET===================================
+        self.entWordToAdd = Entry(title_frame, font=('Arial', 12, 'bold'),
+                                  textvariable=word_to_add,
+                                  bd=5, justify='left', width=25)
+        self.entWordToAdd.grid(row=0, column=1, sticky=W, padx=5)
+        self.entDescription = Entry(title_frame, width=25, bd=5,
+                                    font=('Arial', 12, 'bold'), justify='left',
+                                    textvariable=word_description)
+        self.entDescription.grid(row=1, column=1, sticky=W, padx=5)
+
+        # ======================TREEVIEW WIDGET================================
+        db_columns = ['id', 'Word', 'Meaning']
+        # Set the treeview
+        self.word_database = ttk.Treeview(data_frame, height=50,
+                                          selectmode="extended",
+                                          columns=db_columns)
+
+        # Adding Vertical Scrollbar to Treeview
+        scroll_y = Scrollbar(data_frame, orient=VERTICAL,
+                             command=self.word_database.yview)
+        scroll_y.pack(side=RIGHT, fill=Y)
+        self.word_database.configure(yscrollcommand=scroll_y.set)
+
+        # Adding Horizontal Scrollbar to Treeview
+        scroll_x = Scrollbar(data_frame, orient=HORIZONTAL,
+                             command=self.word_database.xview)
+        scroll_x.pack(side=BOTTOM, fill=X)
+        self.word_database.configure(xscrollcommand=scroll_x.set)
+
+        # Assigning the heading names to the respective columns
+        self.word_database.heading("#0", text="Sr No", anchor=W)
+        self.word_database.heading("#1", text="Keyword", anchor=W)
+        self.word_database.heading("#2", text="Description", anchor=W)
+
+        # Assigning the width to respective columns
+        self.word_database.column('#0', stretch='NO', minwidth=50, width=50)
+        self.word_database.column("#1", stretch='NO', width=100, minwidth=80)
+        self.word_database.column("#2", stretch='NO', width=500, minwidth=100)
+
+        # Calling pack method w.r.to treeview
+        self.word_database.pack(side='top', fill=BOTH, expand=1)
+
+        # ======================BUTTONS WIDGET===================================
+        # Add Button
+        add_button = Button(title_frame, text="Add Data", command=self.addData,
+                            font=('Arial', 8, 'bold'), height=2, width=10, bd=4)
+        add_button.grid(row=0, column=2, padx=2, pady=2, sticky="nsew")
+
+        # Delete Button
+        del_button = Button(title_frame, text="Delete Data",
+                            font=('Arial', 8, 'bold'), height=2, width=10,
+                            bd=4, command=self.delete_data)
+        del_button.grid(row=1, column=2, padx=2, pady=2, sticky="nsew")
+
+        # Search Button
+        search_button = Button(title_frame, text="Search Data",
+                               font=('Arial', 8, 'bold'), height=2, width=10,
+                               bd=4, command=self.searchData)
+        search_button.grid(row=0, column=3, padx=2, pady=2, sticky="nsew")
+
+        # Clear Button
+        clr_button = Button(title_frame, text="Clear Data",
+                            font=('Arial', 8, 'bold'), height=2, width=10,
+                            bd=4, command=self.clearData)
+        clr_button.grid(row=1, column=3, padx=2, pady=2, sticky="nsew")
+
+        # Exit Button
+        exit_button = Button(title_frame, text="Exit",
+                             font=('Arial', 8, 'bold'), height=2, width=10,
+                             bd=4, command=self.Exit_App)
+        exit_button.grid(row=1, column=4, padx=2, pady=2, sticky="nsew")
+
+    # ======================FUNCTION DECLARATION===============================
+    def populateView(self):
+        self.word_database.delete(*self.word_database.get_children())
+        for data in self.dbms_inst.fetch():
+            print(data)  # print all records in the database
+            self.word_database.insert(parent='', index='end', iid=self.iid,
+                                      text=self.id, values=(data[1], data[2]))
+            self.id += 1
+            self.iid += 1
+
+    def addData(self):
+        word = self.entWordToAdd.get()
+        meaning = self.entDescription.get()
+
+        if word == "" or meaning == "":
+            messagebox.showerror("Error", "Enter correct data !!")
+            return
+        else:
+            try:
+                self.dbms_inst.insert(word, meaning)
+                messagebox.showinfo("GUI Dictionary", "Record Added")
+            except Exception:
+                messagebox.showinfo("Message", traceback.print_exc())
+            finally:
+                self.clearData()
+                self.populateView()
+
+    def delete_data(self):
+        row_id = int(self.word_database.focus())
+        result = messagebox.askyesno("GUI Dictionary",
+                                     "Are you sure you want to delete?", icon="warning")
+        if result > 0:
+            self.word_database.delete(row_id)
+
+    def clearData(self):
+        self.entWordToAdd.delete(0, END)
+        self.entDescription.delete(0, END)
+
+    def searchData(self):
+        pass
+
+    def Exit_App(self):
+        result = messagebox.askyesno("GUI Dictionary",
+                                     "Do you want to exit?", icon="warning")
+        if result > 0:
+            self.root.destroy()
+            exit()
 
 
-# Reset Button
-r = Button(window, text="RESET", bg="skyblue", width=10, command=reset)
-r.grid(row=1,column=5)
-
-# Exit Button
-ex = Button(window, text="EXIT", bg="skyblue", width=10, command=exit)
-ex.grid(row=1,column=6)
-
-
-window.mainloop()
+if __name__ == '__main__':
+    window = Tk()
+    application = ConfigureGUI(window)
+    application.populateView()
+    # Start program
+    window.mainloop()
